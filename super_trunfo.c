921c437bbd1208b49378db2285d02aa614d3fc1c
Victor Parizio <victorparizio@hotmail.com>
2025-08-13 21:56:52 -0300

    feat: adicionar variáveis aos atributos das cartas
    - Cada carta possui um conjunto de variáveis
    - Cada conjunto possui uma numeração no final do nome da variável

---
+++ super_trunfo.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+  // Atributos da Carta 1
+  char Estado1;
+  char CodigoDaCarta1[4];
+  char NomeDaCidade1[20];
+  int Populacao1;
+  float Area1;
+  float PIB1;
+  int NumeroDePontosTuristicos1;
+
+  // Atributos da Carta 2
+  char Estado2;
+  char CodigoDaCarta2[4];
+  char NomeDaCidade2[20];
+  int Populacao2;
+  float Area2;
+  float PIB2;
+  int NumeroDePontosTuristicos2;
+}
